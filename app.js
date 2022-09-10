2e529c7a122d5898d409b7ec74f12bc6c4c14ecf
ahmadpato <mhidayaturachman@gmail.com>
2022-09-10 09:14:20 +0700

    fixing Cross-Origin Request Blocked

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use('/api/report', reportRouter);
 app.use('/api/partner', partnerRouter);
 app.use('/api/video', videoRouter);
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+} else {
+    next();
+}});
+
 //set error middleware
 app.use(errorHandler);
 
