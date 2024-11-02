baea0a6aa430b8db9690e5f9fc102a1e797a89b1
ahmadpato <mhidayaturachman@gmail.com>
2024-11-02 21:47:59 +0700

    end point price online class

--- controllers/price-online-class-controller.js
+++ controllers/price-online-class-controller.js
@@ -1,4 +1,4 @@
-const { insertPrice, getPrices, updatePrice, deletePrice } = require('../models/price-model');
+const { insertPriceOnlineClass, getPricesOnlineClass, updatePriceOnlineClass, deletePriceOnlineClass } = require('../models/price-online-class-model');
 const { validatePrice } = require ('../utils/validation');
 const ErrorResponse = require('../utils/errorResponse');
 const { response } = require('express');
@@ -7,7 +7,7 @@ const { response } = require('express');
 exports.createData = (req, res, next) => {
     //buat variable penampung data dan query sql
     const data = {...req.body};
-    const querySql = 'INSERT INTO cms_price SET ?';
+    const querySql = 'INSERT INTO cms_price_online_class SET ?';
     
     //validasi
     var errors = validatePrice(data);
@@ -16,36 +16,36 @@ exports.createData = (req, res, next) => {
     }
 
     // masukan ke dalam model
-    insertPrice(res, querySql, data, next);
+    insertPriceOnlineClass(res, querySql, data, next);
 };
 
 //show Prices
 exports.readData = (req,res,next) => {
     //buat query sql
-    const querySql = 'SELECT * FROM cms_price';
+    const querySql = 'SELECT * FROM cms_price_online_class';
 
     //masukan ke dalam model
-    getPrices(res, querySql, next);
+    getPricesOnlineClass(res, querySql, next);
 }
 
 //update Price
 exports.updateData = (req,res,next) => {
     //buat variabel penampung data dan query sql
     const data = { ...req.body};
-    const querySearch = 'SELECT * FROM cms_price WHERE id  = ?';
-    const queryUpdate = 'UPDATE cms_price SET ? WHERE id = ?';
+    const querySearch = 'SELECT * FROM cms_price_online_class WHERE id  = ?';
+    const queryUpdate = 'UPDATE cms_price_online_class SET ? WHERE id = ?';
 
     //masukan ke dalam model
-    updatePrice(res, querySearch, queryUpdate, req.params.id, data, next);
+    updatePriceOnlineClass(res, querySearch, queryUpdate, req.params.id, data, next);
 }
 
 //delete Price
 exports.deleteData = (req, res, next) => {
     //buat query sql untuk mencari data dan hapus
-    const querySearch = 'SELECT * FROM cms_price WHERE id = ?';
-    const queryDelete = 'DELETE FROM cms_price WHERE id = ?';
+    const querySearch = 'SELECT * FROM cms_price_online_class WHERE id = ?';
+    const queryDelete = 'DELETE FROM cms_price_online_class WHERE id = ?';
 
     //masukan ke dalam model
-    deletePrice(res, querySearch, queryDelete, req.params.id, next);
+    deletePriceOnlineClass(res, querySearch, queryDelete, req.params.id, next);
 };
 
